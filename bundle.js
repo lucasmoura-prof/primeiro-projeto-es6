033e9d209c382d3d47b07440f1061ffd003a2e94
Lucas Moura <lucasmoura.iftm@gmail.com>
2020-03-12 10:27:38 -0300

    Desestruturacao em objetos

--- bundle.js
+++ bundle.js
@@ -1,15 +1,30 @@
 "use strict";
 
-function somar(valorA) {
-  var valorB = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
-  console.log(valorA + valorB);
+var usuario = {
+  nome: 'Jordan',
+  idade: 24,
+  endereco: {
+    cidade: 'Ituiutaba',
+    estado: 'MG'
+  }
+};
+/*console.log(usuario);
+
+let nome = usuario.nome;
+let idade = usuario.idade;
+let cidade = usuario.endereco.cidade;
+
+console.log(nome, idade, cidade);*/
+
+var nome = usuario.nome,
+    idade = usuario.idade,
+    cidade = usuario.endereco.cidade;
+console.log(nome, idade, cidade);
+
+function mostrarNome(_ref) {
+  var nome = _ref.nome,
+      idade = _ref.idade;
+  console.log(nome, idade);
 }
 
-somar(10, 11);
-
-var soma = function soma(valorA) {
-  var valorB = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
-  return valorA + valorB;
-};
-
-console.log(soma(5));
+mostrarNome(usuario);
--- main.js
+++ main.js
@@ -1,9 +1,25 @@
-function somar (valorA, valorB = 10) {
-    console.log(valorA + valorB);
+const usuario = {
+    nome: 'Jordan',
+    idade: 24,
+    endereco: {
+        cidade: 'Ituiutaba',
+        estado: 'MG'
+    }
+};
+
+/*console.log(usuario);
+
+let nome = usuario.nome;
+let idade = usuario.idade;
+let cidade = usuario.endereco.cidade;
+
+console.log(nome, idade, cidade);*/
+
+const { nome, idade, endereco: { cidade } } = usuario;
+console.log(nome, idade, cidade);
+
+function mostrarNome({ nome, idade }) {
+    console.log(nome, idade)
 }
 
-somar(10, 11);
-
-const soma = (valorA, valorB = 10) => valorA + valorB;
-
-console.log(soma(5));
+mostrarNome(usuario);
