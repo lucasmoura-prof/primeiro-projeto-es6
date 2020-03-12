200e6ed159b6fbc1698164d36fcc3a0e0d8d9bbf
Lucas Moura <lucasmoura.iftm@gmail.com>
2020-03-12 10:17:51 -0300

    Valores padrão de parâmetros de função

--- bundle.js
+++ bundle.js
@@ -1,69 +1,15 @@
 "use strict";
 
-// Arrows Functions
-var array = [1, 3, 4, 5, 8, 9];
-var novoArray = array.map(function (item) {
-  return item * 2;
-});
-/*
-    1. Remove a keyword function
-    2. Colocar a flecha => entre o parentese e a chave
-    3. A função recebe parâmetro?
-        Sim
-            3.1 remove o parentese por volta do parametro
-            3.2 a função retorna apenas uma informação (1 linha retorno)
-            3.3 colocar diretamente depois da flecha (sem chaves)
-*/
-
-var teste = function teste() {
-  console.log('oi'); // NÃO É RECOMENDADO FAZER ISSO!!!!!!!!!!
-};
-
-var teste2 = function teste2() {
-  return [1, 2, 3];
-}; // retorna um array 
-
-
-var teste3 = function teste3() {
-  return {
-    nome: 'Joao das Neves',
-    idade: 10
-  };
-}; //Reduce: consumir todo o vetor e transformar em uma única informação (geralmente numero)
-
-
-var soma = array.reduce(function (total, proximoValor) {
-  return total + proximoValor;
-});
-console.log(soma);
-/*
-// Filter: filtrar somente os itens que queremos
-const filterPares = array.filter(function (item) {
-    return item % 2 === 0;
-});
-
-const usuarios = [
-    { nome: 'Usuario 1', idade: 10 },
-    { nome: 'Usuario 2', idade: 15 },
-    { nome: 'Usuario 3', idade: 18 },
-];
-
-const novosUsuarios = usuarios.filter(function (usuario) {
-    return usuario.idade >= 15;
-});
-
-console.log(filterPares);
-
-// Find
-const find = usuarios.find(function (item) {
-    return item.nome === 'Usuario 1';
-});
-
-if (find) {
-    console.log(find);
+function somar(valorA) {
+  var valorB = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
+  console.log(valorA + valorB);
 }
 
-// forEach
-usuarios.forEach(function (item, i) {
-    console.log(item);
-});*/
+somar(10, 11);
+
+var soma = function soma(valorA) {
+  var valorB = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 10;
+  return valorA + valorB;
+};
+
+console.log(soma(5));
--- main.js
+++ main.js
@@ -1,63 +1,9 @@
-// Arrows Functions
-const array = [1, 3, 4, 5, 8, 9];
-
-
-const novoArray = array.map(item => item * 2);
-
-
-/*
-    1. Remove a keyword function
-    2. Colocar a flecha => entre o parentese e a chave
-    3. A função recebe parâmetro?
-        Sim
-            3.1 remove o parentese por volta do parametro
-            3.2 a função retorna apenas uma informação (1 linha retorno)
-            3.3 colocar diretamente depois da flecha (sem chaves)
-*/
-
-const teste = () => {
-    console.log('oi'); // NÃO É RECOMENDADO FAZER ISSO!!!!!!!!!!
+function somar (valorA, valorB = 10) {
+    console.log(valorA + valorB);
 }
 
-const teste2 = () => [1, 2, 3]; // retorna um array 
+somar(10, 11);
 
-const teste3 = () => ({ nome: 'Joao das Neves', idade: 10 });
+const soma = (valorA, valorB = 10) => valorA + valorB;
 
-
-
-
-//Reduce: consumir todo o vetor e transformar em uma única informação (geralmente numero)
-const soma = array.reduce((total, proximoValor) => total + proximoValor);
-
-console.log(soma);
-/*
-// Filter: filtrar somente os itens que queremos
-const filterPares = array.filter(function (item) {
-    return item % 2 === 0;
-});
-
-const usuarios = [
-    { nome: 'Usuario 1', idade: 10 },
-    { nome: 'Usuario 2', idade: 15 },
-    { nome: 'Usuario 3', idade: 18 },
-];
-
-const novosUsuarios = usuarios.filter(function (usuario) {
-    return usuario.idade >= 15;
-});
-
-console.log(filterPares);
-
-// Find
-const find = usuarios.find(function (item) {
-    return item.nome === 'Usuario 1';
-});
-
-if (find) {
-    console.log(find);
-}
-
-// forEach
-usuarios.forEach(function (item, i) {
-    console.log(item);
-});*/
+console.log(soma(5));
