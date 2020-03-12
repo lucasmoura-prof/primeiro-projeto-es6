a4c51fb328819762742793fd8feb0fceee269f74
Lucas Moura <lucasmoura.iftm@gmail.com>
2020-03-12 09:02:05 -0300

    Criando vaiaveis com const e let

--- bundle.js
+++ bundle.js
@@ -1,99 +1,68 @@
 "use strict";
 
-function _typeof(obj) { "@babel/helpers - typeof"; if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+// Declarar variáveis em ES6+
 
-function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+/*
+    const: cria uma variável do tipo constante. 
 
-function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+    Só poderá alterar o valor definido inicialmene
+    atraves de mutações (em objetos ou arrays).
+*/
+var nomeVariavel = "Joao"; // objeto
 
-function _get(target, property, receiver) { if (typeof Reflect !== "undefined" && Reflect.get) { _get = Reflect.get; } else { _get = function _get(target, property, receiver) { var base = _superPropBase(target, property); if (!base) return; var desc = Object.getOwnPropertyDescriptor(base, property); if (desc.get) { return desc.get.call(receiver); } return desc.value; }; } return _get(target, property, receiver || target); }
+var usuario = {
+  nome: 'Jesus',
+  idade: 26
+}; //usuario = { nome: 'Maria', idade: 30 }; // Vai dar erro
+// Permite a mutação do objeto mas não a substituição
 
-function _superPropBase(object, property) { while (!Object.prototype.hasOwnProperty.call(object, property)) { object = _getPrototypeOf(object); if (object === null) break; } return object; }
+usuario.nome = 'Maria';
+var meuVetor = [1, 2, 3, 4, 5]; //meuVetor = [90, 93]; // Vai dar erro
+// Permite a mutação do array através das alterações nos indices
 
-function _getPrototypeOf(o) { _getPrototypeOf = Object.setPrototypeOf ? Object.getPrototypeOf : function _getPrototypeOf(o) { return o.__proto__ || Object.getPrototypeOf(o); }; return _getPrototypeOf(o); }
+meuVetor[0] = 90;
+meuVetor[2] = 93;
+console.log(usuario);
+console.log(meuVetor); // Variável do tipo let
 
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+var valor = 3;
+var nome = 'Claudia Let';
+var altura = 1.86;
+var contemValor = true; //console.log()
+// escopo A
 
-function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+if (true) {// escopo B
+}
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+function somar() {// escopo C
+}
 
-function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+for (var i = 0; i < 5; i++) {
+  // escopo D
+  var teste = 'Maria';
+} // escopo A
 
-function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-var List = /*#__PURE__*/function () {
-  function List() {
-    _classCallCheck(this, List);
-
-    this.data = [];
-  }
-
-  _createClass(List, [{
-    key: "add",
-    value: function add(value) {
-      this.data.push(value);
-      this.print();
-    }
-  }, {
-    key: "pop",
-    value: function pop() {
-      this.data.pop();
-      this.print();
-    }
-  }, {
-    key: "print",
-    value: function print() {
-      console.log(this.data);
-    }
-  }]);
-
-  return List;
-}();
-
-var TodoList = /*#__PURE__*/function (_List) {
-  _inherits(TodoList, _List);
-
-  function TodoList() {
-    _classCallCheck(this, TodoList);
-
-    return _possibleConstructorReturn(this, _getPrototypeOf(TodoList).apply(this, arguments));
-  }
-
-  _createClass(TodoList, [{
-    key: "print",
-    value: function print() {
-      console.log("Imprimindo todoList");
-
-      _get(_getPrototypeOf(TodoList.prototype), "print", this).call(this);
-    }
-  }]);
-
-  return TodoList;
-}(List);
-
-var Matematica = /*#__PURE__*/function () {
-  function Matematica() {
-    _classCallCheck(this, Matematica);
-  }
-
-  _createClass(Matematica, null, [{
-    key: "soma",
-    value: function soma(valueA, valueB) {
-      return valueA + valueB;
-    }
-  }]);
-
-  return Matematica;
-}();
-
-var minhaLista = new TodoList();
-console.log(Matematica.soma(5, 7));
-
-document.getElementById('novotodo').onclick = function () {
-  minhaLista.add('Qualquer coisa');
+var exibirMensagem = function exibirMensagem() {
+  // Hoisting
+  console.log(mensagem);
+  var mensagem = 'Minha mensagem';
+  console.log(mensagem);
 };
 
-document.getElementById('remover').onclick = function () {
-  minhaLista.pop();
-};
+exibirMensagem();
+
+function exibirMensagem2() {
+  // escopo E
+  if (true) {
+    // escopoF
+    var _escopoFuncao = 'Teste';
+    var _escopoBloco = 'Teste 2';
+    console.log(_escopoBloco);
+  }
+
+  console.log(escopoFuncao);
+  console.log(escopoBloco);
+}
+
+exibirMensagem2();
--- main.js
+++ main.js
@@ -1,44 +1,78 @@
-class List {
-    constructor() {
-        this.data = [];
-    }
+// Declarar variáveis em ES6+
 
-    add(value) {
-        this.data.push(value);
-        this.print();
-    }
+/*
+    const: cria uma variável do tipo constante. 
 
-    pop() {
-        this.data.pop();
-        this.print();
-    }
+    Só poderá alterar o valor definido inicialmene
+    atraves de mutações (em objetos ou arrays).
+*/
 
-    print() {
-        console.log(this.data);
-    }
+const nomeVariavel = "Joao";
+
+// objeto
+const usuario = { nome: 'Jesus', idade: 26 };
+
+//usuario = { nome: 'Maria', idade: 30 }; // Vai dar erro
+
+// Permite a mutação do objeto mas não a substituição
+usuario.nome = 'Maria';
+
+const meuVetor = [1, 2, 3, 4, 5];
+
+//meuVetor = [90, 93]; // Vai dar erro
+
+// Permite a mutação do array através das alterações nos indices
+meuVetor[0] = 90;
+meuVetor[2] = 93;
+
+console.log(usuario);
+console.log(meuVetor);
+
+
+// Variável do tipo let
+let valor = 3;
+let nome = 'Claudia Let';
+let altura = 1.86;
+let contemValor = true;
+
+//console.log()
+
+// escopo A
+
+if (true) {
+    // escopo B
 }
 
+function somar() {
+    // escopo C
+}
 
-class TodoList extends List {
-    print() {
-        console.log("Imprimindo todoList");
-        super.print();
+for(var i = 0; i < 5; i++) {
+    // escopo D
+    var teste = 'Maria';
+}
+
+// escopo A
+var exibirMensagem = function () {
+    // Hoisting
+    console.log(mensagem);
+    var mensagem = 'Minha mensagem';
+    console.log(mensagem);
+}
+
+exibirMensagem();
+
+function exibirMensagem2  () {
+    // escopo E
+    if (true) {
+        // escopoF
+        let escopoFuncao = 'Teste';
+        let escopoBloco = 'Teste 2';
+        console.log(escopoBloco);
     }
+    console.log(escopoFuncao);
+    console.log(escopoBloco);
 }
+exibirMensagem2();
 
-class Matematica {
-    static soma(valueA, valueB) {
-        return valueA + valueB;
-    }
-}
 
-const minhaLista = new TodoList();
-console.log(Matematica.soma(5, 7));
-
-document.getElementById('novotodo').onclick = function () {
-    minhaLista.add('Qualquer coisa');
-}
-
-document.getElementById('remover').onclick = function () {
-    minhaLista.pop();
-}
